6feb8aa6d69fb31896ded8ad78029e1328b3df43
Rulo <andres@vitau.mx>
2023-12-03 16:31:12 -0500

    feat(Home): adding course fixes

--- packages/nextjs/components/home/hero/Hero.tsx
+++ packages/nextjs/components/home/hero/Hero.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import MaxWidthWrapper from "../../MaxWidthWrapper";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
+  const router = useRouter();
   return (
-    <form className={styles.hero}>
+    <div className={styles.hero}>
       <div className={styles.blur} />
       <MaxWidthWrapper>
         <div className=" relative z-10 flex flex-col items-start justify-center w-full max-w-[500px] lg:items-center ">
@@ -15,18 +16,17 @@ export const Hero = () => {
           <section className="text-stone-50 text-base font-semibold leading-6 w-full mt-8">
             Descubre un aprendizaje entretenido con recompensas en nuestra app blockchain.
           </section>
-          <Link href="/cursos">
-            <button
-              className="text-stone-50 text-sm font-bold leading-5 whitespace-nowrap justify-center bg-red-600 w-full mt-7 pl-11 pr-16 py-3 rounded-[30px] items-start max-md:px-5"
-              aria-label="Empezar"
-              role="button"
-            >
-              Empezar
-            </button>
-          </Link>
+          <button
+            className="text-stone-50 text-sm font-bold leading-5 whitespace-nowrap justify-center bg-red-600 w-full mt-7 pl-11 pr-16 py-3 rounded-[30px] items-start max-md:px-5"
+            aria-label="Empezar"
+            role="button"
+            onClick={() => router.push("/cursos")}
+          >
+            Empezar
+          </button>
         </div>
       </MaxWidthWrapper>
-    </form>
+    </div>
   );
 };
 
--- packages/nextjs/components/home/new-courses/NewCourses.tsx
+++ packages/nextjs/components/home/new-courses/NewCourses.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Course_1 from "../../../public/course_1.png";
 import styles from "./NewCourses.module.css";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 export const NewCourses = () => {
+  const router = useRouter();
+
   const sliderBreakpoints = {
     320: {
       // when window width is >= 320px
@@ -37,15 +40,18 @@ export const NewCourses = () => {
                 <header className="text-stone-800 text-base font-bold leading-4">Blockchain para principiantes</header>
                 <div className="text-stone-800 text-sm leading-5 grow whitespace-nowrap flex justify-between w-full">
                   2h 50min
-                  <div className="justify-center items-center bg-red-600 self-stretch flex aspect-[2.2222222222222223] flex-col px-6 py-1.5 rounded-[30px] max-md:px-5 text-white">
+                  <button
+                    onClick={() => router.push("/cursos")}
+                    className="justify-center items-center bg-red-600 self-stretch flex aspect-[2.2222222222222223] flex-col px-6 py-1.5 rounded-[30px] max-md:px-5 text-white"
+                  >
                     Play
                     {/* <Image
                     loading="lazy"
                     src="https://cdn.builder.io/api/v1/image/assets/TEMP/03c7eb2138e0feb36d317aeda5b51720a092f0daf37834ad59526081cbfded59?apiKey=d627bfd8f24e41d3baf6fe092e7f3e3f&"
                     alt="Button Icon"
                     className="aspect-square object-contain object-center w-4 overflow-hidden"
-                  /> */}
-                  </div>
+                    /> */}
+                  </button>
                 </div>
               </div>
             </div>
